22b974e392a6e6a32d08362132a94f2e43df0772
Felix Suchert <dev@felixsuchert.de>
2023-04-18 14:46:06 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,46 @@
+# `dfg-mlir`
+
+This repository implements an MLIR dialect for representing dataflow graphs.
+
+## Building
+
+The `dfg-mlir` project is built using **CMake** (version `3.20` or newer). Make sure to provide all dependencies required by the project, either by installing them to system-default locations, or by setting the appropriate search location hints!
+
+### Using `nix`
+
+To build the dialect on a Nix-based system, you can use the provided `flake.nix` file to get a development shell up and running.
+Make sure that you have [enabled Flakes](https://nixos.wiki/wiki/Flakes#Enable_flakes) for your Nix installation (verifiable by calling `nix flake` from the command line).
+
+Then, just run `nix develop` to get a shell that provides LLVM and MLIR versions working for this project.
+Build the dialect by running
+
+```bash
+# Configure.
+cmake -S . -B build $cmakeFlags
+# Build.
+cmake --build build
+```
+
+### Manually
+
+Make sure, you have MLIR and LLVM built and available on your system. Then point CMake to the include directories as shown below:
+
+```sh
+# Configure.
+cmake -S . -B build \
+    -G Ninja \
+    -DLLVM_DIR=$LLVM_PREFIX/lib/cmake/llvm \
+    -DMLIR_DIR=$MLIR_PREFIX/lib/cmake/mlir
+
+# Build.
+cmake --build build
+```
+
+The following CMake variables can be configured:
+
+|       Name | Type     | Description |
+| ---------: | :------- | --- |
+| `LLVM_DIR` | `STRING` | Path to the CMake directory of an **LLVM** installation. <br/> *e.g. `~/tools/llvm-17/lib/cmake/llvm`* |
+| `MLIR_DIR` | `STRING` | Path to the CMake directory of an **MLIR** installation. <br/> *e.g. `~/tools/llvm-17/lib/cmake/mlir`* |
+
+
---
+++ flake.nix
@@ -0,0 +1,104 @@
+{
+  description = "An over-engineered Hello World in C";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-22.11";
+    mlir = {
+      url = "github:Feliix42/mlir.nix/main";
+      #url = "github:Feliix42/mlir.nix/circt";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, mlir }:
+    let
+
+      # to work with older version of flakes
+      lastModifiedDate = self.lastModifiedDate or self.lastModified or "19700101";
+
+      # Generate a user-friendly version number.
+      version = builtins.substring 0 8 lastModifiedDate;
+
+      # System types to support.
+      supportedSystems = [ "x86_64-linux" ]; # "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
+
+      # Helper function to generate an attrset '{ x86_64-linux = f "x86_64-linux"; ... }'.
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+
+      # Nixpkgs instantiated for supported system types.
+      nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; overlays = [ self.overlay ]; });
+
+    in
+
+    {
+
+      # A Nixpkgs overlay.
+      overlay = final: prev: {
+
+        dfg_dialect = with final; final.callPackage ({ inShell ? false }: llvmPackages_14.stdenv.mkDerivation rec {
+          pname = "dfg-mlir";
+          inherit version;
+
+          # need no copy of the source tree in the Nix Store when using `nix develop`
+          src = if inShell then null else ./.;
+
+          nativeBuildInputs = [
+            python3
+            ninja
+            cmake
+            llvmPackages_14.clang
+            llvmPackages_14.bintools
+            mlir.packages.x86_64-linux.mlir
+            lit
+          ];
+
+          # buildInputs = (if inShell then [
+          #   # in the `nix develop` shell, we also want:
+          # ]);
+
+          cmakeFlags = [
+            "-GNinja"
+            "-DMLIR_DIR=${mlir}/lib/cmake/mlir"
+            "-DLLVM_DIR=${mlir}/lib/cmake/llvm"
+
+            # Debug for debug builds
+            #"-DCMAKE_BUILD_TYPE=RelWithDebInfo"
+            # this makes llvm only to produce code for the current platform, this saves CPU time, change it to what you need
+            #"-DLLVM_TARGETS_TO_BUILD=X86"
+            # NOTE(feliix42): THIS IS ABI BREAKING!!
+            #"-DLLVM_ENABLE_ASSERTIONS=ON"
+            "-DCMAKE_EXPORT_COMPILE_COMMANDS=ON"
+            # Using clang and lld speeds up the build, we recomment adding:
+            "-DCMAKE_C_COMPILER=clang"
+            "-DCMAKE_CXX_COMPILER=clang++"
+            "-DLLVM_ENABLE_LLD=ON"
+            "-DLLVM_EXTERNAL_LIT=${lit}/bin/lit"
+          ];
+        }) {};
+      };
+
+      # Provide some binary packages for selected system types.
+      packages = forAllSystems (system:
+        {
+          inherit (nixpkgsFor.${system}) dfg_dialect;
+        });
+
+      # The default package for 'nix build'. This makes sense if the
+      # flake provides only one package or there is a clear "main"
+      # package.
+      defaultPackage = forAllSystems (system: self.packages.${system}.dfg_dialect);
+
+      # Provide a 'nix develop' environment for interactive hacking.
+      devShell = forAllSystems (system: self.packages.${system}.dfg_dialect.override { inShell = true; });
+
+      # A NixOS module, if applicable (e.g. if the package provides a system service).
+      nixosModules.dfg_dialect =
+        { pkgs, ... }:
+        {
+          nixpkgs.overlays = [ self.overlay ];
+
+          environment.systemPackages = [ pkgs.dfg_dialect ];
+        };
+
+    };
+}
