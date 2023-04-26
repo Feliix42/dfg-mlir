89fdc4a910bb5e0e8ab55beb6d6722a44aef0a1c
Felix Suchert <dev@felixsuchert.de>
2023-04-26 16:47:18 +0200

    Add LSP, fix Type definition

---
+++ tools/dfg-lsp-server/dfg-lsp-server.cpp
@@ -0,0 +1,27 @@
+/// Main entry point for the dfg-mlir MLIR language server.
+///
+/// @file
+/// @author     Felix Suchert (felix.suchert@tu-dresden.de)
+
+#include "dfg-mlir/Dialect/dfg/IR/Base.h"
+#include "mlir/IR/Dialect.h"
+#include "mlir/IR/MLIRContext.h"
+#include "mlir/InitAllDialects.h"
+#include "mlir/Tools/mlir-lsp-server/MlirLspServerMain.h"
+
+using namespace mlir;
+
+static int asMainReturnCode(LogicalResult r)
+{
+    return r.succeeded() ? EXIT_SUCCESS : EXIT_FAILURE;
+}
+
+int main(int argc, char* argv[])
+{
+    DialectRegistry registry;
+    registerAllDialects(registry);
+
+    registry.insert<dfg::DfgDialect>();
+
+    return asMainReturnCode(MlirLspServerMain(argc, argv, registry));
+}
