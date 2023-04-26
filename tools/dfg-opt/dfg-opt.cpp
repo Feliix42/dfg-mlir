993a30272d5595a8d0033319b207d70716a46d73
Felix Suchert <dev@felixsuchert.de>
2023-04-26 16:50:38 +0200

    Add dfg-opt

---
+++ tools/dfg-opt/dfg-opt.cpp
@@ -0,0 +1,26 @@
+/// Main entry point for the dfg-mlir optimizer driver.
+///
+/// @file
+/// @author     Felix Suchert (felix.suchert@tu-dresden.de)
+
+#include "dfg-mlir/Dialect/dfg/IR/Base.h"
+#include "mlir/IR/Dialect.h"
+#include "mlir/IR/MLIRContext.h"
+#include "mlir/InitAllDialects.h"
+#include "mlir/InitAllPasses.h"
+#include "mlir/Tools/mlir-opt/MlirOptMain.h"
+
+using namespace mlir;
+
+int main(int argc, char* argv[])
+{
+    DialectRegistry registry;
+    registerAllDialects(registry);
+
+    registry.insert<dfg::DfgDialect>();
+
+    registerAllPasses();
+
+    return asMainReturnCode(
+        MlirOptMain(argc, argv, "dfg-mlir optimizer driver\n", registry));
+}
