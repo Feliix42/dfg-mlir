b9c273eae71f22e2a75d17036a445e57840c7472
Felix Suchert <dev@felixsuchert.de>
2023-05-04 09:59:20 +0200

    Add printer/parser stub

--- lib/Dialect/dfg/IR/Ops.cpp
+++ lib/Dialect/dfg/IR/Ops.cpp
@@ -7,6 +7,7 @@
 #include "dfg-mlir/Dialect/dfg/IR/Dialect.h"
 
 #include "mlir/IR/OpImplementation.h"
+#include "mlir/IR/FunctionImplementation.h"
 #include "mlir/IR/PatternMatch.h"
 #include "mlir/IR/TypeUtilities.h"
 
@@ -22,6 +23,23 @@ using namespace mlir::dfg;
 
 //===----------------------------------------------------------------------===//
 
+ParseResult OperatorOp::parse(OpAsmParser &parser, OperationState &result) {
+  auto buildFuncType =
+      [](Builder &builder, ArrayRef<Type> argTypes, ArrayRef<Type> results,
+         function_interface_impl::VariadicFlag,
+         std::string &) { return builder.getFunctionType(argTypes, results); };
+
+  return function_interface_impl::parseFunctionOp(
+      parser, result, /*allowVariadic=*/false,
+      getFunctionTypeAttrName(result.name), buildFuncType,
+      getArgAttrsAttrName(result.name), getResAttrsAttrName(result.name));
+}
+
+void OperatorOp::print(OpAsmPrinter &p) {
+  function_interface_impl::printFunctionOp(
+      p, *this, /*isVariadic=*/false, getFunctionTypeAttrName(),
+      getArgAttrsAttrName(), getResAttrsAttrName());
+}
 
 //===----------------------------------------------------------------------===//
 // DfgDialect
