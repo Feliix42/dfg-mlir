936d382e5dcddee23757584f82aa090708dd08b2
Felix Suchert <dev@felixsuchert.de>
2023-06-05 18:12:34 +0200

    Rewrite parser for operator

--- lib/Dialect/dfg/IR/Ops.cpp
+++ lib/Dialect/dfg/IR/Ops.cpp
@@ -31,13 +31,14 @@ using namespace mlir::dfg;
 //===----------------------------------------------------------------------===//
 
 void OperatorOp::build(OpBuilder &builder, OperationState &state, StringRef name,
-                   ArrayRef<NamedAttribute> inp_attrs,
-                   ArrayRef<NamedAttribute> out_attrs) {
+                       FunctionType type) {
+
     state.addAttribute(SymbolTable::getSymbolAttrName(),
                        builder.getStringAttr(name));
-    state.attributes.append(inp_attrs.begin(), inp_attrs.end());
-    state.attributes.append(out_attrs.begin(), out_attrs.end());
+    state.addAttribute(getFunctionTypeAttrName(state.name), TypeAttr::get(type));
     state.addRegion();
+
+    // FIXME(feliix42): addArgAndResultAttrs to register the function arguments as block arguments (?) -> maybe also not needed, we'll see
 }
 
 // temporary workaround
@@ -46,54 +47,113 @@ bool OperatorOp::isExternal() {
     return body.empty();
 }
 
-// TODO(feliix42): Change the parser to actually parse the format that I
-//                 envision for the operator semantics
-ParseResult OperatorOp::parse(OpAsmParser &parser, OperationState &result) {
-//   auto buildFuncType =
-//       [](Builder &builder, ArrayRef<Type> argTypes, ArrayRef<Type> results,
-//          function_interface_impl::VariadicFlag,
-//          std::string &) { return builder.getFunctionType(argTypes, results); };
+static ParseResult
+parseFunctionArgumentList(OpAsmParser &parser, 
+                          SmallVectorImpl<OpAsmParser::Argument> &arguments) {
+    return parser.parseCommaSeparatedList(
+        OpAsmParser::Delimiter::Paren, [&]() -> ParseResult {
+            // // Handle ellipsis as a special case.
+            // if (succeeded(parser.parseOptionalEllipsis())) {
+            //   // This is a variadic designator.
+            //   return failure(); // Stop parsing arguments.
+            // }
 
-//   return function_interface_impl::parseFunctionOp(
-//       parser, result, /*allowVariadic=*/false,
-//       getFunctionTypeAttrName(result.name), buildFuncType,
-//       getArgAttrsAttrName(result.name), getResAttrsAttrName(result.name));
-    return ParseResult::failure();
+            // Parse argument name if present.
+            OpAsmParser::Argument argument;
+            auto argPresent = parser.parseOptionalArgument(
+                argument, /*allowType=*/true, /*allowAttrs=*/false);
+            if (argPresent.has_value()) {
+                if (failed(argPresent.value()))
+                    return failure(); // Present but malformed.
+
+                // Reject this if the preceding argument was missing a name.
+                if (!arguments.empty() && arguments.back().ssaName.name.empty())
+                    return parser.emitError(argument.ssaName.location,
+                                            "expected type instead of SSA identifier");
+
+            } else {
+                argument.ssaName.location = parser.getCurrentLocation();
+                // Otherwise we just have a type list without SSA names.  Reject
+                // this if the preceding argument had a name.
+                if (!arguments.empty() && !arguments.back().ssaName.name.empty())
+                    return parser.emitError(argument.ssaName.location,
+                                            "expected SSA identifier");
+
+                NamedAttrList attrs;
+                if (parser.parseType(argument.type) ||
+                    parser.parseOptionalAttrDict(attrs) ||
+                    parser.parseOptionalLocationSpecifier(argument.sourceLoc))
+                    return failure();
+                argument.attrs = attrs.getDictionary(parser.getContext());
+            }
+            arguments.push_back(argument);
+            return success();
+        }
+    );
+}
+
+ParseResult OperatorOp::parse(OpAsmParser &parser, OperationState &result) {
+    auto &builder = parser.getBuilder();
+
+    // parse the operator name
+    StringAttr nameAttr;
+    if (parser.parseSymbolName(nameAttr, SymbolTable::getSymbolAttrName(), result.attributes))
+        return failure();
+
+    // parse the signature of the operator
+    SmallVector<OpAsmParser::Argument> arguments, outputArgs;
+    SMLoc signatureLocation = parser.getCurrentLocation();
+
+    // parse inputs/outputs separately for later distinction
+    if (succeeded(parser.parseKeyword("inputs"))) {
+        if (parseFunctionArgumentList(parser, arguments))
+            return failure();
+    }
+
+    if (succeeded(parser.parseKeyword("outputs"))) {
+        if (parseFunctionArgumentList(parser, outputArgs))
+            return failure();
+    }
+
+    SmallVector<Type> argTypes;
+    argTypes.reserve(arguments.size());
+    SmallVector<Type> resultTypes;
+    resultTypes.reserve(outputArgs.size());
+
+    for (auto &arg: arguments)
+        argTypes.push_back(arg.type);
+    for (auto &arg: outputArgs)
+        resultTypes.push_back(arg.type);
+    Type type = builder.getFunctionType(argTypes, resultTypes);
+
+    if (!type) {
+        return parser.emitError(signatureLocation) << "Failed to construct operator type";
+    }
+
+    result.addAttribute(getFunctionTypeAttrName(result.name), TypeAttr::get(type));
+
+    // merge both argument lists for the block arguments
+    arguments.append(outputArgs);
+
+
+    auto *body = result.addRegion();
+    SMLoc loc = parser.getCurrentLocation();
+    OptionalParseResult parseResult = parser.parseOptionalRegion(*body, arguments, /*enableNameShadowing=*/false);
+
+    if (parseResult.has_value()) {
+        if (failed(*parseResult))
+            return failure();
+        if (body->empty())
+            return parser.emitError(loc, "expected non-empty operator body");
+    }
+    return success();
 }
 
 void OperatorOp::print(OpAsmPrinter &p) {
     // print the operation and function name
-    Operation* op = getOperation();
-    
-    auto funcName =
-        op->getAttrOfType<StringAttr>(SymbolTable::getSymbolAttrName())
-            .getValue();
-    p << ' ';
+    // Operation* op = getOperation();
 
-    p.printSymbolName(funcName); 
-    auto input_args = op->getAttr("input_attrs");
-    auto output_args = op->getAttr("output_attrs");
-
-    // only print the inputs and outputs lists when there's attributes inside
-    if (input_args) {
-        p << " inputs (";
-        p.printAttribute(input_args);
-        p << ')';
-    }
-
-    if (input_args && output_args) {
-        // you only need this when there's both inputs and outputs
-        p.printNewline();
-        p.increaseIndent();
-    }
-
-    if (output_args) {
-        p << "outputs (";
-        p.printAttribute(output_args);
-        p << ')';
-    }
-
-    p.printRegion(getRegion());
+    // CONTINUE
 }
 
 //===----------------------------------------------------------------------===//
