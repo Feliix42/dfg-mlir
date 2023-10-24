4747f47cf4d1a64f49736c5e032b1f2fbede49fe
Felix Suchert <dev@felixsuchert.de>
2023-10-24 15:11:20 +0200

    Implement LoopOp Lowering (close #9)

--- lib/Conversion/DfgToFunc/DfgToFunc.cpp
+++ lib/Conversion/DfgToFunc/DfgToFunc.cpp
@@ -65,20 +65,9 @@ struct OperatorOpLowering : public OpConversionPattern<OperatorOp> {
                 genFuncOp.getBody(),
                 genFuncOp.end());
 
-            // add a terminator block to the function
-            Block* terminatorBlock = rewriter.createBlock(
-                &genFuncOp.getBody(),
-                genFuncOp.getBody().end());
-
-            // insert the cf.br operation at the end of the previous block
-            rewriter.setInsertionPointToEnd(&genFuncOp.getBody().front());
-            auto branchOp = rewriter.create<cf::BranchOp>(
-                genFuncOp.getBody().front().back().getLoc(),
-                terminatorBlock);
-
             // populate the Terminator Block (for now) with a return only
-            rewriter.setInsertionPointToEnd(terminatorBlock);
-            rewriter.create<func::ReturnOp>(branchOp.getLoc());
+            rewriter.setInsertionPointToEnd(&genFuncOp.getRegion().back());
+            rewriter.create<func::ReturnOp>(op.getLoc());
         }
 
         rewriter.eraseOp(op);
@@ -111,6 +100,88 @@ struct InstantiateOpLowering : public OpConversionPattern<InstantiateOp> {
     }
 };
 
+struct LoopOpLowering : public OpConversionPattern<LoopOp> {
+    using OpConversionPattern<LoopOp>::OpConversionPattern;
+
+    LoopOpLowering(TypeConverter &typeConverter, MLIRContext* context)
+            : OpConversionPattern<LoopOp>(typeConverter, context){};
+
+    LogicalResult matchAndRewrite(
+        LoopOp op,
+        LoopOpAdaptor adaptor,
+        ConversionPatternRewriter &rewriter) const override
+    {
+        // TODO(feliix42):
+        // - test that this warning is triggered!
+
+        // Warn when not all channels are watched
+        llvm::DenseSet<Value> regionArgs;
+        Region &parentRegion = op.getRegion();
+        for (auto item : parentRegion.getArguments()) regionArgs.insert(item);
+
+        // FIXME(feliix42): What about channels created in the Loop (before the
+        // loop is not possible)
+        for (auto inItem : adaptor.getInChans()) regionArgs.erase(inItem);
+        for (auto outItem : adaptor.getOutChans()) regionArgs.erase(outItem);
+
+        if (!regionArgs.empty())
+            emitWarning(
+                op.getLoc(),
+                "This loop does not watch all channels used by the operator. "
+                "However, in the software lowering the closure of *any* "
+                "channel in the executed code path will lead to the "
+                "termination of the loop and therefore the operator. This may "
+                "lead to behavior that differs from the Hardware lowering");
+
+        // 1. get teardown block (for later reference)
+        // maybe remove this assert? I'm depending on it for finding the
+        // terminator block, though
+        assert(parentRegion.getBlocks().size() == 1);
+        Block &terminatorBlock = parentRegion.getBlocks().back();
+
+        // 2. split after the loopOp -> finalizerBlock
+        Block* currentBlock = op->getBlock();
+        rewriter.setInsertionPointAfter(op);
+        Block* finalizerBlock =
+            rewriter.splitBlock(currentBlock, rewriter.getInsertionPoint());
+
+        // 3. re-wire the cond_br in the loop to point to the finalizer
+        SmallVector<cf::CondBranchOp> rewritableBranches;
+        op->walk([&](cf::CondBranchOp branchOp) {
+            if (branchOp.getFalseDest() == &terminatorBlock)
+                rewritableBranches.push_back(branchOp);
+        });
+
+        for (auto rewriteOp : rewritableBranches) {
+            rewriter.replaceOpWithNewOp<cf::CondBranchOp>(
+                rewriteOp,
+                rewriteOp.getCondition(),
+                rewriteOp.getTrueDest(),
+                rewriteOp.getTrueOperands(),
+                finalizerBlock,
+                ArrayRef<Value>());
+        }
+
+        // 3.5 insert the back-jump at the end of the loop (while I still can find it)
+        Region& loopRegion = op.getRegion();
+        Block* loopEntry = &loopRegion.front();
+        rewriter.setInsertionPointToEnd(&loopRegion.back());
+        rewriter.create<cf::BranchOp>(op.getLoc(), &loopRegion.front());
+
+        // 4. move the blocks from the region to after the LoopOp
+        rewriter.inlineRegionBefore(loopRegion, finalizerBlock);
+
+        // 5. insert a br before the loop
+        rewriter.setInsertionPointToEnd(currentBlock);
+        rewriter.create<cf::BranchOp>(op.getLoc(), loopEntry);
+
+        // 6. delete the current op
+        rewriter.eraseOp(op);
+
+        return success();
+    }
+};
+
 void mlir::populateDfgToFuncConversionPatterns(
     TypeConverter typeConverter,
     RewritePatternSet &patterns)
@@ -120,6 +191,9 @@ void mlir::populateDfgToFuncConversionPatterns(
 
     // dfg.instantiate -> func.call
     patterns.add<InstantiateOpLowering>(typeConverter, patterns.getContext());
+
+    // dfg.loop -> cf
+    patterns.add<LoopOpLowering>(typeConverter, patterns.getContext());
 }
 
 void ConvertDfgToFuncPass::runOnOperation()
@@ -139,7 +213,7 @@ void ConvertDfgToFuncPass::runOnOperation()
         LLVM::LLVMDialect>();
 
     target.addLegalDialect<DfgDialect>();
-    target.addIllegalOp<OperatorOp>();
+    target.addIllegalOp<OperatorOp, LoopOp>();
     target.addDynamicallyLegalOp<InstantiateOp>(
         [](InstantiateOp op) { return op.getOffloaded(); });
 
