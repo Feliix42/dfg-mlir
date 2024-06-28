86cf6c66acd92989feddd1f117f47b25215ee622
HahaLan97 <jiahong.bi@gmail.com>
2024-06-28 19:31:38 +0200

    Update Operator2Yaml Pass.

--- lib/Dialect/dfg/Transforms/PrintOperatorToYaml/PrintOperatorToYaml.cpp
+++ lib/Dialect/dfg/Transforms/PrintOperatorToYaml/PrintOperatorToYaml.cpp
@@ -3,10 +3,13 @@
 /// @file
 /// @author     Jiahong Bi (jiahong.bi@tu-dresden.de)
 
+#include "dfg-mlir/Conversion/Utils.h"
 #include "dfg-mlir/Dialect/dfg/IR/Dialect.h"
 #include "dfg-mlir/Dialect/dfg/IR/Ops.h"
 #include "dfg-mlir/Dialect/dfg/IR/Types.h"
 #include "dfg-mlir/Dialect/dfg/Transforms/Passes.h"
+#include "mlir/Dialect/Arith/IR/Arith.h"
+#include "mlir/Dialect/SCF/IR/SCF.h"
 #include "mlir/IR/PatternMatch.h"
 #include "mlir/Transforms/DialectConversion.h"
 
@@ -41,6 +44,7 @@ struct GraphNodePort {
 
 struct GraphNode {
     std::string name;
+    std::string type;
     std::vector<GraphNodePort> ports;
     int execCycles;
 };
@@ -58,6 +62,11 @@ struct GraphYaml {
     GraphInfo graphInfo;
     std::vector<GraphNode> nodes;
     std::vector<GraphChannel> channels;
+};
+
+struct MappingInfo {
+    std::string nodeName;
+    GraphNodePort port;
 };
 
 namespace llvm {
@@ -85,6 +94,7 @@ struct MappingTraits<GraphNode> {
     static void mapping(IO &io, GraphNode &graphNode)
     {
         io.mapRequired("name", graphNode.name);
+        io.mapRequired("type", graphNode.type);
         io.mapRequired("ports", graphNode.ports);
         io.mapRequired("exec_cycles", graphNode.execCycles);
     }
@@ -164,28 +174,255 @@ namespace {
 struct DfgPrintOperatorToYamlPass
         : public dfg::impl::DfgPrintOperatorToYamlBase<
               DfgPrintOperatorToYamlPass> {
+public:
     void runOnOperation() override;
+
+private:
+    std::vector<GraphNode> getGraphNodes(OperatorOp operatorOp);
+    void getGraphNodesFromOp(
+        Operation* op,
+        std::vector<GraphNode> &nodes,
+        size_t idxBias);
+    std::vector<GraphChannel> getGraphChannels();
+
+    bool isConstant(Operation* op) { return (isa<arith::ConstantOp>(op)); }
+    bool isArithNode(Operation* op)
+    {
+        return (
+            isa<arith::AddIOp>(op) || isa<arith::SubIOp>(op)
+            || isa<arith::MulIOp>(op) || isa<arith::CmpIOp>(op));
+    }
+    bool isMuxNode(Operation* op) { return (isa<scf::IfOp>(op)); }
+    void addValueIntoMap(Value value, std::vector<MappingInfo> maps)
+    {
+        auto it = valueToPorts.find(value);
+        if (it != valueToPorts.end()) {
+            // Key exists, push the new port into the vector
+            it->second.insert(it->second.end(), maps.begin(), maps.end());
+        } else {
+            // Key does not exist, create a new entry with the key and a vector
+            // containing the port
+            valueToPorts[value] = maps;
+        }
+    }
+
+    llvm::DenseMap<Value, std::vector<MappingInfo>> valueToPorts;
+    SmallVector<Value> constants;
+    llvm::DenseMap<Value, Value> yieldToOutput;
+    int arithNodeIdx = 0;
+    int muxNodeIdx = 0;
+    mlir::Region::BlockArgListType blkArgs;
+    SmallVector<Value> debug_results;
 };
 } // namespace
+
+std::vector<GraphNode>
+DfgPrintOperatorToYamlPass::getGraphNodes(OperatorOp operatorOp)
+{
+    std::vector<GraphNode> nodes;
+    auto funcTy = operatorOp.getFunctionType();
+    blkArgs = operatorOp.getBody().getArguments();
+
+    // Each input will be a source node
+    // If the input is used more than once, multiple output port will be created
+    for (size_t i = 0; i < funcTy.getNumInputs(); i++) {
+        auto nodeName = "arg" + std::to_string(i);
+        auto blkArg = blkArgs[i];
+        auto blkArgUses = blkArg.getUses();
+        std::vector<GraphNodePort> ports;
+        std::vector<MappingInfo> maps;
+        for (auto j = 0;
+             j < std::distance(blkArgUses.begin(), blkArgUses.end());
+             j++) {
+            auto portName = nodeName + '_' + std::to_string(j);
+            GraphNodePort port = {portName, "out"};
+            ports.push_back(port);
+            maps.push_back({nodeName, port});
+        }
+        // The execution cycles for src nodes should be defined by hardware
+        GraphNode node = {nodeName, "src", ports, 1};
+        addValueIntoMap(blkArg, maps);
+        nodes.push_back(node);
+    }
+
+    // Each operation will be handled
+    auto idxBias = funcTy.getNumInputs();
+    for (auto &op : operatorOp.getOps())
+        getGraphNodesFromOp(&op, nodes, idxBias);
+
+    // Each output will be a sink node
+    for (size_t i = 0; i < funcTy.getNumResults(); i++) {
+        auto idx = i + idxBias;
+        auto nodeName = "arg" + std::to_string(idx);
+        auto blkArg = blkArgs[idx];
+        std::vector<GraphNodePort> ports;
+        std::vector<MappingInfo> maps;
+        // The execution cycles for src nodes should be defined by hardware
+        GraphNodePort port = {nodeName, "in"};
+        ports.push_back(port);
+        maps.push_back({nodeName, port});
+        GraphNode node = {nodeName, "sink", ports, 1};
+        addValueIntoMap(yieldToOutput[blkArg], maps);
+        nodes.push_back(node);
+    }
+
+    return nodes;
+}
+
+void DfgPrintOperatorToYamlPass::getGraphNodesFromOp(
+    Operation* op,
+    std::vector<GraphNode> &nodes,
+    size_t idxBias)
+{
+    if (isConstant(op)) {
+        constants.push_back(op->getResult(0));
+    } else if (isArithNode(op)) {
+        auto nodeName = "arith_" + std::to_string(arithNodeIdx++) + '_'
+                        + op->getName().getStringRef().str();
+        std::vector<GraphNodePort> ports;
+
+        auto lhs = op->getOperand(0);
+        auto isLhsCst = isInSmallVector<Value>(lhs, constants);
+        auto rhs = op->getOperand(1);
+        auto isRhsCst = isInSmallVector<Value>(rhs, constants);
+        assert(!(isLhsCst && isRhsCst) && "Please do constant folding first");
+        GraphNodePort lhsPort, rhsPort;
+        if (!isLhsCst) {
+            lhsPort = {"lhs", "in"};
+            addValueIntoMap(
+                lhs,
+                std::vector<MappingInfo>{
+                    {nodeName, lhsPort}
+            });
+            ports.push_back(lhsPort);
+        }
+        if (!isRhsCst) {
+            rhsPort = {"rhs", "in"};
+            addValueIntoMap(
+                rhs,
+                std::vector<MappingInfo>{
+                    {nodeName, rhsPort}
+            });
+            ports.push_back(rhsPort);
+        }
+
+        auto result = op->getResult(0);
+        auto resultUses = result.getUses();
+        std::vector<GraphNodePort> resultPorts;
+        std::vector<MappingInfo> maps;
+        for (auto j = 0;
+             j < std::distance(resultUses.begin(), resultUses.end());
+             j++) {
+            auto portName = "result_" + std::to_string(j);
+            GraphNodePort port = {portName, "out"};
+            resultPorts.push_back(port);
+            maps.push_back({nodeName, port});
+        }
+        addValueIntoMap(result, maps);
+        ports.insert(ports.end(), resultPorts.begin(), resultPorts.end());
+        GraphNode node = {nodeName, "arith", ports, 1};
+        nodes.push_back(node);
+
+        debug_results.push_back(result);
+    } else if (isMuxNode(op)) {
+        // TODO: deal with multiple results
+        auto nodeName = "mux_" + std::to_string(muxNodeIdx++);
+        std::vector<GraphNodePort> ports;
+        auto select = op->getOperand(0);
+        GraphNodePort selectPort = {"sel", "in"};
+        addValueIntoMap(
+            select,
+            std::vector<MappingInfo>{
+                {nodeName, selectPort}
+        });
+        ports.push_back(selectPort);
+
+        int regionNum = 0;
+        for (auto &region : op->getRegions())
+            for (auto &op : region.getOps())
+                if (isa<scf::YieldOp>(op)) {
+                    auto yield = op.getOperand(0);
+                    auto portName = "yield_" + std::to_string(regionNum++);
+                    GraphNodePort yieldPort = {portName, "in"};
+                    addValueIntoMap(
+                        yield,
+                        std::vector<MappingInfo>{
+                            {nodeName, yieldPort}
+                    });
+                    ports.push_back(yieldPort);
+                } else
+                    getGraphNodesFromOp(&op, nodes, idxBias);
+
+        auto result = op->getResult(0);
+        auto resultUses = result.getUses();
+        std::vector<GraphNodePort> resultPorts;
+        std::vector<MappingInfo> maps;
+        for (auto j = 0;
+             j < std::distance(resultUses.begin(), resultUses.end());
+             j++) {
+            auto portName = "result_" + std::to_string(j);
+            GraphNodePort port = {portName, "out"};
+            resultPorts.push_back(port);
+            maps.push_back({nodeName, port});
+        }
+        addValueIntoMap(result, maps);
+        ports.insert(ports.end(), resultPorts.begin(), resultPorts.end());
+        GraphNode node = {nodeName, "mux", ports, 1};
+        nodes.push_back(node);
+
+        debug_results.push_back(result);
+    } else if (isa<scf::YieldOp>(op)) {
+        return;
+    } else if (isa<YieldOp>(op)) {
+        for (size_t i = 0; i < op->getNumOperands(); i++)
+            yieldToOutput[blkArgs[idxBias + i]] = op->getOperand(i);
+    }
+}
+
+std::vector<GraphChannel> DfgPrintOperatorToYamlPass::getGraphChannels()
+{
+    std::vector<GraphChannel> channels;
+    int channelNum = 0;
+    for (auto map : valueToPorts) {
+        std::vector<MappingInfo> producers, consumers;
+        for (auto mapInfo : map.second)
+            if (mapInfo.port.type == "in")
+                consumers.push_back(mapInfo);
+            else if (mapInfo.port.type == "out")
+                producers.push_back(mapInfo);
+        assert(producers.size() == consumers.size());
+        for (size_t i = 0; i < producers.size(); i++) {
+            channels.push_back(
+                {"ch" + std::to_string(channelNum++),
+                 producers[i].nodeName,
+                 producers[i].port.name,
+                 consumers[i].nodeName,
+                 consumers[i].port.name,
+                 0});
+        }
+    }
+    return channels;
+}
 
 void DfgPrintOperatorToYamlPass::runOnOperation()
 {
     auto module = dyn_cast<ModuleOp>(getOperation());
 
-    // TODO: get nodes and channels from an operator
-    std::vector<GraphNode> nodes = {
-        {    "src",                  {{"output", "out"}}, 1},
-        {"compute", {{"input", "in"}, {"output", "out"}}, 1},
-        {   "sink",                    {{"input", "in"}}, 1}
-    };
-    std::vector<GraphChannel> channels = {
-        {"ch0",     "src", "output", "compute", "input", 0},
-        {"ch1", "compute", "output",    "sink", "input", 1}
-    };
-
     module.walk([&](OperatorOp operatorOp) {
+        assert(
+            !operatorOp.getBody().empty()
+            && "Cannot print anything out of an empty operator.");
+        for (auto &op : operatorOp.getOps())
+            if (!isa<arith::ConstantOp>(op) && !isa<arith::AddIOp>(op)
+                && !isa<arith::SubIOp>(op) && !isa<arith::MulIOp>(op)
+                && !isa<arith::CmpIOp>(op) && !isa<scf::IfOp>(op)
+                && !isa<scf::YieldOp>(op) && !isa<YieldOp>(op))
+                signalPassFailure();
+
         auto graphName = operatorOp.getSymName().str();
-        GraphYaml graph = {{graphName}, nodes, channels};
+        auto graphNodes = getGraphNodes(operatorOp);
+        auto graphChannels = getGraphChannels();
+        GraphYaml graph = {{graphName}, graphNodes, graphChannels};
         auto fileName = "graph_" + graphName + ".yaml";
 
         // Emit YAML into separate files
